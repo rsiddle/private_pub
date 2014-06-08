b0157496d9c9b76e301c4cbf9b57a18385abd00d
Cameron Martin <cameronmartin123@gmail.com>
2014-06-08 23:43:40 +0100

    Cast expires env variable to integer

--- lib/private_pub/load_config.rb
+++ lib/private_pub/load_config.rb
@@ -1,3 +1,3 @@
 PrivatePub.config[:secret_token] = ENV['PRIVATE_PUB_SECRET_TOKEN']
-PrivatePub.config[:signature_expiration] = ENV['PRIVATE_PUB_EXPIRES']
+PrivatePub.config[:signature_expiration] = Integer(ENV['PRIVATE_PUB_EXPIRES'])
 PrivatePub.config[:server] = ENV['PRIVATE_PUB_SERVER']

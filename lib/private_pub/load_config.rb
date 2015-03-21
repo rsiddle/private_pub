6462c458ce31ff149f5f94cbde760acd59b3acda
Ryan Siddle <ryansiddle@googlemail.com>
2015-03-21 18:47:35 +0000

    Update location

--- lib/private_pub/load_config.rb
+++ lib/private_pub/load_config.rb
@@ -1,4 +1,4 @@
-YAML.load_file(File.join("/www", "shared", "private_pub", "config.yml"))
+vars = YAML.load_file(File.join("/www", "shared", "private_pub", "config.yml"))
 env_mode = ENV['RACK_ENV'] || "production"
 PrivatePub.config[:secret_token] = vars[env_mode]["secret_token"]
 PrivatePub.config[:signature_expiration] = vars[env_mode]["signature_expiration"].to_i

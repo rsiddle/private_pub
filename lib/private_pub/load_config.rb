3065726f1c6da86322657864473d6d1b975d0c7b
Ryan Siddle <ryansiddle@googlemail.com>
2015-03-21 18:43:28 +0000

    Add hard link

--- lib/private_pub/load_config.rb
+++ lib/private_pub/load_config.rb
@@ -1,5 +1,5 @@
-env_mode = ENV["RACK_ENV"] || "production"
-vars = YAML.load_file(File.join("/www", env_mode, "www", "current", "config", "private_pub", "config.yml"))
+YAML.load_file(File.join("/www", "shared", "private_pub", "config.yml"))
+env_mode = ENV['RACK_ENV'] || "production"
 PrivatePub.config[:secret_token] = vars[env_mode]["secret_token"]
 PrivatePub.config[:signature_expiration] = vars[env_mode]["signature_expiration"].to_i
 PrivatePub.config[:server] = vars[env_mode]["server"]
